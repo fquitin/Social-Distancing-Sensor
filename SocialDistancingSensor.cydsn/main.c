06516afde6c9315957cddb3e0ce7aaee4da06ae3
Sami <aaa0050@gmail.com>
2020-07-08 08:59:36 +0200

    Working distance measure

--- SocialDistancingSensor.cydsn/main.c
+++ SocialDistancingSensor.cydsn/main.c
@@ -44,7 +44,6 @@ enum STATE_e {
     STATE_TAG_GET_TIMES,
     STATE_TAG_PROCESS_DISTANCE,
     STATE_TAG_END,
-    
     /* Anchor */
     STATE_ANCHOR_LISTENING,
     STATE_ANCHOR_SEND_ANSWER,
@@ -52,29 +51,6 @@ enum STATE_e {
     STATE_ANCHOR_WAIT_FINAL,
     STATE_ANCHOR_SEND_TIMES,
     STATE_ANCHOR_END,
-    
-    /* Social */
-    STATE_INITIATE_CONTACT,
-    STATE_INITIATE_CONTACT_PENDING,
-    STATE_PREPARE_TO_LISTEN,
-    STATE_LISTENING,
-    STATE_SEND_FINAL,
-    STATE_SEND_FINAL_PENDING,
-    STATE_WAIT_FOR_RESULTS,
-    STATE_PROCESS_RESULTS,
-    STATE_ANSWER,
-    STATE_ANSWER_PENDING,
-    STATE_WAIT_FOR_FINAL,
-    STATE_SEND_RESULTS,
-    STATE_SEND_RESULTS_PENDING,
-};
-
-enum Message_ID_e {
-    INITIAL_MSG = 0x10,
-    //ANSWER_MSG = 0x11,
-    ANSWER_MSG = 0xFA,
-    FINAL_MSG = 0x12,
-    RESULTS_MSG = 0x13,
 };
 
 enum STATE_e state = STATE_INIT;
@@ -95,11 +71,6 @@ uint8_t SW4_prev = 0;
 uint8_t RxOk = 0u;
 uint8_t TxOk = 0u;
 uint8_t RxError = 0u;
-uint32_t cycleTimeoutFlag = 0u;
-uint16_t cycleTimeout = 0u;
-uint16_t messageTimeout = 0u;
-#define CYCLE_TIMEOUT (500u)
-#define MESSAGE_TIMEOUT (5u)
 
 static uint64_t t1, t2, t3, t4, t5, t6;
 static uint8_t t1_8[5];
@@ -127,7 +98,7 @@ double distance;
 double tof_num;
 double tof_denum;
 
-#define MEAN_LEN (1u)
+#define MEAN_LEN (8u)
 double distLog[MEAN_LEN];
 uint8_t distIdx = 0;
 double mean = 0.0;
@@ -136,8 +107,6 @@ uint8_t succesRate = 0;
 uint8_t successCntr = 0;
 #define SUCCESS_LEN (20u)
 float successPercent = 0.0f;
-
-#define MAIN_TIMEOUT (500u)
 
 #define TICK2S (1.0f / (128 * 499.2f * 10e6)) * 10 // Unknown factor 10 
 //#define TICK2S (1.0f / (72499865600.0f))
@@ -149,26 +118,10 @@ void rxLoop(void);
 void txLoop(void);
 void anchorLoop(void);
 void tagLoop(void);
-void socialDistancindLoop(void);
 
-CY_ISR( isr_timeout_Handler ){
-    /* Clear the inteerrupt */
-    Timeout_timer_ReadStatusRegister();
-    
-    //timeoutFlag = timeoutFlag > 1u ? timeoutFlag-1u : 0u;
-    
-    if (cycleTimeout == 0u){
-        cycleTimeoutFlag = 1u;
-        cycleTimeout = CYCLE_TIMEOUT;
-    }else{
-        cycleTimeout--;
-    }
-    
-    messageTimeout = messageTimeout > 1u ? messageTimeout-1u : 0u;
-}
 
 CY_ISR( isr_timer_Handler ){
-    
+    /* Clear the inteerrupt */
     Timer_ReadStatusRegister();
     state = STATE_INIT;
     LED1_Write(~LED1_Read());
@@ -212,13 +165,9 @@ int main(void)
     LCD_Position(0u,0u);
     LCD_PrintString("Init..Ok");
 
-    //isr_timeout_StartEx(isr_timer_Handler);
-    //Timer_Start();
+    isr_timer_StartEx(isr_timer_Handler);
+    Timer_Start();
     
-    Timeout_timer_Start();
-    isr_timeout_StartEx(isr_timeout_Handler);
-    
-    socialDistancindLoop();
     
     if (Pot_Read())
     {
@@ -227,6 +176,15 @@ int main(void)
     {
         role = TAG;
     }
+    
+    /*
+    while(role == NOT_DEFINED){
+        if(SW1_Read()){ role = RECEIVER; }
+        if(SW2_Read()){ role = TAG; }
+        if(SW3_Read()){ role = ANCHOR; }
+        if(SW4_Read()){ role = TRANSMITTER; }
+    }
+    */
     
     if (role == TRANSMITTER){ txLoop(); }
     else if(role == RECEIVER){ rxLoop(); }
@@ -472,21 +430,27 @@ void tagLoop(){
     LCD_Position(0u, 0u);
     LCD_PrintString("tag     ");
 
+    
+    
     for(;;){
         switch(state){
             case STATE_INIT :
-
+//                SW3_prev = SW3_cur;
+//                SW3_cur = SW3_Read();
                 LCD_Position(1,6);
                 LCD_PrintNumber(successPercent);
 
                 if (1)
-
+                //if (SW3_cur && !(SW3_prev))
                 {
-
+//                    CyDelay(50u);
+//                    LCD_Position(5u, 0u);
+//                    LCD_PrintString("sent");
                     idle();
                     TxData[0] = 0xFA;
                     DWM_SendData(TxData,1,1);
                     state = STATE_TAG_WAIT_FIRST_SEND;
+
                 }
             break;
                 
@@ -504,7 +468,10 @@ void tagLoop(){
             break;
                 
     		case STATE_TAG_WAIT_RESPONSE :
-                if(inter){ interrupt_routine(); }
+                if(inter){ 
+                    //CyDelay(10u);
+                    interrupt_routine(); 
+                }
                 if (RxError){
                     RxError = 0;
                     DWM_Reset_Rx();
@@ -633,242 +600,6 @@ void tagLoop(){
             break;
         }
     }
-}
-
-void socialDistancindLoop(){
-    //Timeout_timer_Start();
-    state = STATE_INITIATE_CONTACT;
-    for (;;){
-        switch(state){
-            case STATE_INITIATE_CONTACT :
-                LED1_Write(~LED1_Read());
-                idle();
-                TxData[0] = INITIAL_MSG;
-                DWM_SendData(TxData, 1, 1);
-                state = STATE_INITIATE_CONTACT_PENDING;
-                messageTimeout = MESSAGE_TIMEOUT;
-                break;
-            
-            case STATE_INITIATE_CONTACT_PENDING:
-                if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; }  
-                if(inter){ interrupt_routine(); }
-                if (TxOk){
-                    TxOk = 0;
-                    DWM_ReadSPI_ext(TX_TIME, NO_SUB, t1_8, 5);
-                    state = STATE_PREPARE_TO_LISTEN;
-                }
-                break;
-                
-            case STATE_PREPARE_TO_LISTEN:
-                idle();
-                DWM_Enable_Rx();
-                state = STATE_LISTENING;
-                break;
-
-            case STATE_LISTENING:    
-
-                if (cycleTimeoutFlag){
-                    cycleTimeoutFlag = 0u;
-                    state = STATE_INITIATE_CONTACT;
-                }
-                
-                if(inter){ interrupt_routine(); }
-
-                if (RxError){
-                    RxError = 0;
-                    DWM_Reset_Rx();
-                    state = STATE_PREPARE_TO_LISTEN;
-                }
-                if (RxOk){
-                    RxOk = 0;
-                    DWM_ReceiveData(RxData);
-                    if (RxData[0] == INITIAL_MSG){
-                        DWM_ReadSPI_ext(RX_TIME,NO_SUB, t2_8,5);
-                        state = STATE_ANSWER;
-                    }
-                    else if(RxData[0] == ANSWER_MSG){
-                        DWM_ReadSPI_ext(RX_TIME,NO_SUB, t4_8,5);
-                        state = STATE_SEND_FINAL;
-                    }else{
-                        state = STATE_LISTENING; // @TODO Verif
-                    }
-                }
-
-                break;
-            
-            case STATE_SEND_FINAL:
-                    TxData[0] = FINAL_MSG;
-                    DWM_SendData(TxData, 1, 1);
-                    state = STATE_SEND_FINAL_PENDING;
-                    messageTimeout = MESSAGE_TIMEOUT;
-                break;
-                
-            case STATE_SEND_FINAL_PENDING:
-                if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; }  
-                if(inter){ interrupt_routine(); }
-                if (TxOk){
-                    TxOk = 0;
-                    DWM_ReadSPI_ext(TX_TIME, NO_SUB, t5_8, 5);
-                    idle();
-                    DWM_Enable_Rx();
-                    state = STATE_WAIT_FOR_RESULTS;
-                    messageTimeout = MESSAGE_TIMEOUT;
-                }
-                break;
-
-            case STATE_WAIT_FOR_RESULTS:
-                if(inter){ interrupt_routine(); }
-                if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; }  
-                if (RxError){
-                    RxError = 0;
-                    state = STATE_PREPARE_TO_LISTEN;  
-                    DWM_Reset_Rx();
-                }
-                if (RxOk){
-                    // @TODO handle first byte of RxData
-                    DWM_ReceiveData(RxData);
-                    for (int i=0;i<5;i++){
-                        t2_8[i] = RxData[i];
-                        t3_8[i] = RxData[i+5];
-                        t6_8[i] = RxData[i+10];
-                    }
-
-                    t1 = t2 = t3 = t4 = t5 = t6 = 0;
-                    for (int i=0;i<5;i++){
-                        t1 = (t1 << 8) | t1_8[4-i];
-                        t2 = (t2 << 8) | t2_8[4-i];
-                        t3 = (t3 << 8) | t3_8[4-i];
-                        t4 = (t4 << 8) | t4_8[4-i];
-                        t5 = (t5 << 8) | t5_8[4-i];
-                        t6 = (t6 << 8) | t6_8[4-i];
-                    }
-                    if (t6 < t2 || t5 < t1){
-                        state = STATE_PREPARE_TO_LISTEN;  
-                    }
-                    else{
-                        state = STATE_PROCESS_RESULTS;
-                        RxOk = 0;
-                    }
-                }
-                break;
-
-            case STATE_PROCESS_RESULTS:
-
-                TroundMaster = (t4-t1);
-                TreplySlave = (t3-t2);
-                TroundSlave = (t6-t3);
-                TreplyMaster = (t5-t4);
-                
-                tof_num = TroundMaster * TroundSlave - TreplyMaster * TreplySlave;
-                tof_denum = TroundMaster + TroundSlave + TreplySlave + TroundMaster;
-                timeOfFly_tick = tof_num /tof_denum; 
-                
-                distance = LIGHT_SPEED * timeOfFly_tick * TICK2S; 
-
-                if (distance > 0.0 && distance < 100.0)
-                {
-                    distLog[distIdx] = distance;
-                    distIdx += 1;
-                    if(distIdx >= MEAN_LEN){
-                        distIdx = 0;
-                        LED4_Write(~LED4_Read());
-                        
-                        mean = 0.0;
-                        for (uint8_t i = 0; i < MEAN_LEN; i++){
-                            mean += distLog[i];
-                            //distLog[i] = 0;
-                        }
-                        mean = mean / MEAN_LEN;
-                        
-                        char str[8];
-                        sprintf(str, "%d cm", (int) (mean * 100));
-                        LCD_ClearDisplay();
-                        LCD_Position(0,0);
-                        LCD_PrintString(str);
-                        
-                    }                    
-                }
-
-                
-                //CyDelay(500u);
-                //while(timeoutFlag > 0u){};
-                
-                state = STATE_PREPARE_TO_LISTEN;
-                
-                
-                break;
-
-                
-            case STATE_ANSWER:
-                idle();
-                TxData[0] = ANSWER_MSG;
-                DWM_SendData(TxData, 1, 1);
-                state = STATE_ANSWER_PENDING;
-                messageTimeout = MESSAGE_TIMEOUT;
-                break;
-            
-            case STATE_ANSWER_PENDING:
-                if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; } 
-                if(inter){ interrupt_routine(); }
-                if (TxOk){
-                    TxOk = 0;
-                    DWM_ReadSPI_ext(TX_TIME, NO_SUB, t3_8, 5);
-                    idle();
-                    DWM_Enable_Rx();
-                    state = STATE_WAIT_FOR_FINAL;
-                    messageTimeout = MESSAGE_TIMEOUT;
-                }
-                break;
-
-            case STATE_WAIT_FOR_FINAL:
-                if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; } 
-                if(inter){ interrupt_routine(); }
-                if (RxError){
-                    RxError = 0;
-                    DWM_Reset_Rx();
-                    state = STATE_PREPARE_TO_LISTEN; 
-                }
-                else if (RxOk){
-                    RxOk = 0;
-                    DWM_ReceiveData(RxData);
-                    DWM_ReceiveData(RxData);
-                    if (RxData[0] == FINAL_MSG){
-                        DWM_ReadSPI_ext(RX_TIME,NO_SUB, t6_8,5);
-                        state = STATE_SEND_RESULTS;
-                    }
-                    else{
-                        state = STATE_PREPARE_TO_LISTEN; 
-                    }
-                }
-                break;
-
-                case STATE_SEND_RESULTS:
-                    idle();
-                    for (int i=0;i<5;i++){
-                        TxData[i] = t2_8[i];
-                        TxData[i+5] = t3_8[i];
-                        TxData[i+10] = t6_8[i];
-                    }
-                    DWM_SendData(TxData, 15, 1);
-                    state = STATE_SEND_RESULTS_PENDING;
-                    messageTimeout = MESSAGE_TIMEOUT;
-                    break;
-                
-                case STATE_SEND_RESULTS_PENDING:
-                    if (messageTimeout == 0u){ state = STATE_PREPARE_TO_LISTEN; }
-                    if(inter){ interrupt_routine(); }
-                    if (TxOk){
-                        TxOk = 0;
-                        LED3_Write(~LED3_Read());
-                        state = STATE_PREPARE_TO_LISTEN;
-                    }
-                    break;
-                
-            default:
-                break;
-        }
-    }
-
 }
 
 
